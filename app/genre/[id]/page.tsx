6c560a499c0a586da797a591ad136d8124246ffd
CodeKeeper <codekeeper@CodeKeepers-MacBook-Pro.local>
2024-09-26 19:43:26 -0400

    build update

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,27 +1,26 @@
-import MoviesCarousel from '@/components/MoviesCarousel';
-import { getDiscoverMovies } from '@/lib/getMovies';
-import React from 'react';
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
+import React from "react";
 
 type Props = {
-    params: {
-        id: number;
-        genre: string;
-    }
-}
+  params: {
+    id: string;
+    genre: string;
+  };
+};
 
-async function GenrePage({ params: { id }, searchParams: { genre }} :Props) {
-
-    const movies = await getDiscoverMovies(id);
+async function GenrePage({ params: { id, genre } }: Props) {
+  const movies = await getDiscoverMovies(id);
 
   return (
-    <div className='max-w-7xl mx-auto'>
-          {/* Azure OpenAI Service Suggestion */}
-          <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
-            <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
-            <MoviesCarousel title={`Genre`} movies={movies} isVerticle/>
-          </div>
+    <div className="max-w-7xl mx-auto">
+      {/* Azure OpenAI Service Suggestion */}
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+        <MoviesCarousel title={`Genre`} movies={movies} isVerticle />
+      </div>
     </div>
-  )
+  );
 }
 
-export default GenrePage
+export default GenrePage;
